3440633fca12eb58939f2c8a72c58e840dcfe8e4
Julio Di Nicola <juliodinicola@gmail.com>
2021-05-24 13:49:03 +0200

    Updated dist

--- dist/index.js
+++ dist/index.js
@@ -3,13 +3,11 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "MicroCalendar", {
-  enumerable: true,
-  get: function get() {
-    return _MicroCalendar.default;
-  }
-});
+exports.default = void 0;
 
 var _MicroCalendar = _interopRequireDefault(require("./components/MicroCalendar"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _default = _MicroCalendar.default;
+exports.default = _default;
